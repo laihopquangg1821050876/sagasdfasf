7dd287a0bb21e65856f355b271be6da7aa125cc9
quanga123 <quangoc0302@gmail.com>
2021-12-13 21:08:43 +0700

    quang

---
+++ Models/VsDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public partial class VsContext : DbContext
+    {
+        public VsContext()
+            : base("name=VsDbContext")
+        {
+        }
+
+        public virtual DbSet<sinhvien> sinhviens { get; set; }
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+
+
+    }
+}
---
+++ Models/sinhvien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [Table("sinhvien")]
+    public class sinhvien
+    {
+        [Key]
+        public int MaSV { get; set; }
+        public string  TenSV { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime namsinh { get; set; }
+
+    }
+}
